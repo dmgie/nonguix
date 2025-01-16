59cda58a72234e70b3aa705f9a7acef26243e344
Murilo <murilo@disroot.org>
2025-01-16 09:22:00 -0300

    nongnu: Do not export mesa/fake-beta.
    * nongnu/packages/nvidia.scm (mesa/fake-beta): Do not export.
    Do not use hidden-package for grafting.

--- nongnu/packages/nvidia.scm
+++ nongnu/packages/nvidia.scm
@@ -847,11 +847,10 @@ variables @code{__GLX_VENDOR_LIBRARY_NAME=nvidia} and
     (inherit mesa)
     (replacement nvda)))
 
-(define-public mesa/fake-beta
-  (hidden-package
-   (package
-     (inherit mesa)
-     (replacement nvdb))))
+(define mesa/fake-beta
+  (package
+    (inherit mesa)
+    (replacement nvdb)))
 
 (define-public replace-mesa
   (package-input-rewriting `((,mesa . ,mesa/fake))))
